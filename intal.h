08288ee57026c7382aaa12026b1dab343ec2874c
lordvishwa123@gmail.com <lord8266>
2020-05-21 22:29:42 +0530

    update to new header

--- intal.h
+++ intal.h
@@ -11,31 +11,43 @@
 // You can write some of your own helper functions, keep them "static".
 // Use the sample test file (that has the main function) to test your implementation.
 
+// To generate your own testcases, you can use https://www.omnicalculator.com/math
+
 // Returns the sum of two intals.
-char* intal_add(char* intal1, char* intal2);
+char* intal_add(const char* intal1, const char* intal2);
 
 // Returns the comparison value of two intals.
 // Returns 0 when both are equal.
 // Returns +1 when intal1 is greater, and -1 when intal2 is greater.
-int intal_compare(char* intal1, char* intal2);
+int intal_compare(const char* intal1, const char* intal2);
 
 // Returns the difference (obviously, nonnegative) of two intals.
-char* intal_diff(char* intal1, char* intal2);
+char* intal_diff(const char* intal1, const char* intal2);
 
 // Returns the product of two intals.
-char* intal_multiply(char* intal1, char* intal2);
+char* intal_multiply(const char* intal1, const char* intal2);
 
 // Returns intal1 mod intal2
 // The mod value should be in the range [0, intal2 - 1].
-char* intal_mod(char* intal1, char* intal2);
+// intal2 > 1
+// Implement a O(log intal1) time taking algorithm.
+// O(intal1 / intal2) time taking algorithm may exceed time limit.
+// O(intal1 / intal2) algorithm may repeatedly subtract intal2 from intal1.
+// That will take intal1/intal2 iterations.
+// You need to design a O(log intal1) time taking algorithm.
+// Generate your own testcases at https://www.omnicalculator.com/math/modulo
+char* intal_mod(const char* intal1, const char* intal2);
 
 // Returns intal1 ^ intal2.
 // Let 0 ^ n = 0, where n is an intal.
-char* intal_pow(char* intal1, unsigned int n);
+// Implement a O(log n) intal multiplications algorithm.
+// 2^3000 has less than 1000 decimal digits. 3000 intal multiplications may exceed time limit.
+char* intal_pow(const char* intal1, unsigned int n);
 
 // Returns Greatest Common Devisor of intal1 and intal2.
-// Let GCD be "0" if one of intal1 and intal2 is "0".
-char* intal_gcd(char* intal1, char* intal2);
+// Let GCD be "0" if both intal1 and intal2 are "0" even though it is undefined, mathematically.
+// Use Euclid's theorem to not exceed the time limit.
+char* intal_gcd(const char* intal1, const char* intal2);
 
 // Returns nth fibonacci number.
 // intal_fibonacci(0) = intal "0".
@@ -47,9 +59,11 @@ char* intal_factorial(unsigned int n);
 
 // Returns the Binomial Coefficient C(n,k).
 // 0 <= k <= n
-// C(n,k) < 10^1000 because the reurning value is expected to be less than 10^1000.
+// C(n,k) < 10^1000 because the returning value is expected to be less than 10^1000.
 // Use the Pascal's identity C(n,k) = C(n-1,k) + C(n-1,k-1)
-// Make sure the intermediate intal values do not cross C(n.k).
+// Make sure the intermediate intal values do not cross C(n,k).
+// Use Dynamic Programming. Use extra space of not more than O(k) number of intals. Do not allocate the whole O(nk) DP table.
+// Don't let C(1000,900) take more time than C(1000,500). Time limit may exceed otherwise.
 char* intal_bincoeff(unsigned int n, unsigned int k);
 
 // Returns the offset of the largest intal in the array.
@@ -65,14 +79,14 @@ int intal_min(char **arr, int n);
 // Returns the offset of the first occurrence of the key intal in the array.
 // Returns -1 if the key is not found.
 // 1 <= n <= 1000
-int intal_search(char **arr, int n, char* key);
+int intal_search(char **arr, int n, const char* key);
 
 // Returns the offset of the first occurrence of the key intal in the SORTED array.
 // Returns -1 if the key is not found.
 // The array is sorted in nondecreasing order.
 // 1 <= n <= 1000
 // The implementation should be a O(log n) algorithm.
-int intal_binsearch(char **arr, int n, char* key);
+int intal_binsearch(char **arr, int n, const char* key);
 
 // Sorts the array of n intals.
 // 1 <= n <= 1000
@@ -84,6 +98,6 @@ void intal_sort(char **arr, int n);
 // The goal is to pick up the maximum amount of money subject to the constraint that
 // no two coins adjacent in the initial row can be picked up.
 // 1 <= n <= 1000
-// The implementation should be O(n) time and O(1) extra space.
+// The implementation should be O(n) time and O(1) extra space even though the DP table may be of O(n) size.
 // Eg: Coins = [10, 2, 4, 6, 3, 9, 5] returns 25
 char* coin_row_problem(char **arr, int n);
